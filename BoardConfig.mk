488a9f349bfe4139f5b85402988705f02d39e53a
SGCMarkus <markusornik@gmail.com>
2019-03-31 09:58:38 +0200

    judypn: initial commit
    * overlay is missing

---
+++ BoardConfig.mk
@@ -0,0 +1,45 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PATH := device/lge/judypn
+
+# inherit from common v30
+-include device/lge/sdm845-common/BoardConfigCommon.mk
+
+TARGET_OTA_ASSERT_DEVICE := v40,judypn,v405ebw,V40
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
+
+# Kernel
+BOARD_KERNEL_CMDLINE += androidboot.hardware=judypn
+TARGET_KERNEL_CONFIG := lineageos_judypn_defconfig
+
+# Partitions
+BOARD_USERDATAIMAGE_PARTITION_SIZE := #TODO
+BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
+
+BOARD_ROOT_EXTRA_FOLDERS := oem vzw firmware persist ftm persdata
+BOARD_ROOT_EXTRA_SYMLINKS := \
+    /vendor/dps:/dsp \
+    /vendor/eri:/eri \
+    /vendor/persdata/absolute:/persdata
+
+# Recovery
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.judypn
+
+# inherit from the proprietary version
+-include vendor/lge/judypn/BoardConfigVendor.mk
---
+++ lineage_judypn.mk
@@ -0,0 +1,52 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+
+# Inherit from judypn device
+$(call inherit-product, device/lge/judypn/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Overlays (inherit after vendor/cm to ensure we override it)
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+# Device identifiers
+
+PRODUCT_NAME := lineage_judypn
+PRODUCT_DEVICE := judypn
+PRODUCT_BRAND := lge
+PRODUCT_MANUFACTURER := LGE
+PRODUCT_RELEASE_NAME := V40
+PRODUCT_MODEL := LM-V405
+
+PRODUCT_GMS_CLIENTID_BASE := android-om-lg
+
+TARGET_VENDOR_PRODUCT_NAME := judypn_lao_eea
+TARGET_VENDOR_DEVICE_NAME := judypn
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=judypn \
+    PRODUCT_NAME=judypn_lao_eea \
+    PRIVATE_BUILD_DESC="judypn_lao_eea-user 8.1.0 OPM1.171019.026 1900910023231 release-keys"
+
+BUILD_FINGERPRINT="lge/judypn_lao_eea/judypn:8.1.0/OPM1.171019.026/1900910023231:user/release-keys"
+
