ea145c72149361390f3c875045ec79b6379cf75a
j0sh1x <aljoshua.hell@gmail.com>
2019-05-12 12:50:56 +0200

    inital upload to github

---
+++ BoardConfig.mk
@@ -0,0 +1,56 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PATH := device/lge/judyln
+
+# inherit from common v30
+-include device/lge/sdm845-common/BoardConfigCommon.mk
+
+#TARGET_OTA_ASSERT_DEVICE := g7,judyln,g710em,G7
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
+
+# Lights
+TARGET_PROVIDES_LIBLIGHT := true
+
+# Kernel
+BOARD_KERNEL_CMDLINE += androidboot.hardware=judyln
+TARGET_KERNEL_CONFIG := judyln_lao_com-perf_defconfig
+TARGET_KERNEL_SOURCE := kernel/lge/sdm845
+#TARGET_NO_KERNEL := true
+#TARGET_PREBUILT_KERNEL := device/lge/judyln/zImage
+
+# Partitions
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 49758027776
+BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
+BOARD_PREBUILT_VENDORIMAGE := device/lge/judyln/vendor.img
+BOARD_PREBUILT_VBMETAIMAGE := $(DEVICE_PATH)/vbmeta.img
+#BOARD_PREBUILT_BOOTIMAGE := $(DEVICE_PATH)/boot.img
+
+BOARD_ROOT_EXTRA_FOLDERS := misc ftm oem
+BOARD_ROOT_EXTRA_SYMLINKS := \
+    /mnt/product/carrier:/carrier \
+    /system/product:/product \
+
+# SELinux policies
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+
+# Recovery
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.judyln
+
+# inherit from the proprietary version
+-include vendor/lge/judyln/BoardConfigVendor.mk
---
+++ lineage_judyln.mk
@@ -0,0 +1,53 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
+
+# Inherit from judyln device
+$(call inherit-product, device/lge/judyln/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+
+# Overlays (inherit after vendor/cm to ensure we override it)
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+# Device identifiers
+
+PRODUCT_NAME := lineage_judyln
+PRODUCT_DEVICE := judyln
+PRODUCT_BRAND := lge
+PRODUCT_MANUFACTURER := LGE
+PRODUCT_RELEASE_NAME := G7
+PRODUCT_MODEL := LM-G710
+
+PRODUCT_GMS_CLIENTID_BASE := android-om-lg
+
+TARGET_VENDOR_PRODUCT_NAME := judyln_lao_com
+TARGET_VENDOR_DEVICE_NAME := judyln
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=judyln \
+    PRODUCT_NAME=judyln_lao_eea \
+    PRIVATE_BUILD_DESC="judyln_lao_com-user 9 PKQ1.181105.001 19007103579e3 release-keys"
+
+BUILD_FINGERPRINT="lge/judyln_lao_com/judyln:9/PKQ1.181105.001/19007103579e3:user/release-keys"
+
