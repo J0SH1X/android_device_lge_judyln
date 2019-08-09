efcddae12579e6dff30af15e23a66e3a537a3bd9
SGCMarkus <markusornik@gmail.com>
2019-08-09 19:18:15 +0200

    judypn: set vendor security patch in BoardConfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,5 +48,8 @@ BOARD_ROOT_EXTRA_SYMLINKS := \
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.judypn
 
+# Security Patch Level
+VENDOR_SECURITY_PATCH := 2019-07-01
+
 # inherit from the proprietary version
 -include vendor/lge/judypn/BoardConfigVendor.mk
--- vendor_prop.mk
+++ vendor_prop.mk
@@ -6,6 +6,3 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.multisim.config=dsds
 
-# Security Patch Level
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.build.security_patch=2019-07-01
