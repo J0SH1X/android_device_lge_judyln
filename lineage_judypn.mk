b755fd60603da6fd10016de634c76e7bdece74ee
SGCMarkus <markusornik@gmail.com>
2019-09-13 09:20:26 +0200

    judypn: include GSI keys

--- lineage_judypn.mk
+++ lineage_judypn.mk
@@ -20,6 +20,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
+# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
 # Inherit from judypn device
 $(call inherit-product, device/lge/judypn/device.mk)
 
