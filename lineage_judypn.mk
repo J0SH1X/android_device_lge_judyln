53d882821d5d5c3a395041cf5b6a026aa00fdbd7
SGCMarkus <markusornik@gmail.com>
2019-09-13 09:21:13 +0200

    judypn: add updatable_apex.mk

--- lineage_judypn.mk
+++ lineage_judypn.mk
@@ -23,6 +23,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Inherit from judypn device
 $(call inherit-product, device/lge/judypn/device.mk)
 
