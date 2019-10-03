443c8b67150267ea5c78e6f78dea21ed039b31ca
SGCMarkus <markusornik@gmail.com>
2019-10-03 15:30:34 +0200

    judypn: nuke framework_compatibility_matrix.xml

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,7 +25,6 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
 
 # Kernel
 BOARD_KERNEL_CMDLINE += androidboot.hardware=judypn
