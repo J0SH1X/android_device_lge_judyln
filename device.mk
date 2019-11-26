17d5c1ae9be5426fec6a3e8e85fc9816fd046fdc
Llama <gregariivanov@gmail.com>
2019-11-26 23:27:02 +0100

    judyln: Fix a typo for implementing vendor blobs
    The path was pointing at a non-extisting location
    thus not copying any vendor blobs into vendor

--- device.mk
+++ device.mk
@@ -17,7 +17,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Get non-open-source specific aspects
-$(call inherit-product-if-exists, vendor/lge/judyln/judypn-vendor.mk)
+$(call inherit-product-if-exists, vendor/lge/judyln/judyln-vendor.mk)
 
 DEVICE_PATH := device/lge/judyln
 
